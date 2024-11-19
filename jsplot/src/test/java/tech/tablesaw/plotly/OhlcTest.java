750f5839af0fd2af3325bf2874610a816841b118
ccleva <christophe.cleva@orange.fr>
2024-11-19 15:31:24 +0100

    Added test cases with Instant and LocalDate

--- jsplot/src/test/java/tech/tablesaw/plotly/OhlcTest.java
+++ jsplot/src/test/java/tech/tablesaw/plotly/OhlcTest.java
@@ -1,12 +1,17 @@
 package tech.tablesaw.plotly;
 
 import org.junit.jupiter.api.Test;
+
+import tech.tablesaw.api.DateColumn;
 import tech.tablesaw.api.DateTimeColumn;
 import tech.tablesaw.api.DoubleColumn;
+import tech.tablesaw.api.InstantColumn;
 import tech.tablesaw.api.Table;
 import tech.tablesaw.plotly.api.OHLCPlot;
 import tech.tablesaw.plotly.components.Figure;
 
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
@@ -16,30 +21,51 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class OhlcTest {
 
-    @Test
-    void ohlcPlotDoesNotThrowIllegalArgumentException() {
-        // Test to fix bug reported at https://github.com/jtablesaw/tablesaw/issues/1237
-        String timeTitle = "time";
-        String openTitle = "open";
-        String closeTitle = "close";
-        String highTitle = "high";
-        String lowTitle = "low";
-        String graphTitle = "title";
+    private static final String timeTitle = "time";
+    private static final String openTitle = "open";
+    private static final String closeTitle = "close";
+    private static final String highTitle = "high";
+    private static final String lowTitle = "low";
+    private static final String graphTitle = "title";
+    private static final List<Double> open = Arrays.asList(1d, 2d, 3d);
+    private static final List<Double> close = Arrays.asList(1d, 2d, 3d);
+    private static final List<Double> high = Arrays.asList(1d, 2d, 3d);
+    private static final List<Double> low = Arrays.asList(1d, 2d, 3d);
+    private static final DoubleColumn openColumn = DoubleColumn.create(openTitle, open);
+    private static final DoubleColumn closeColumn = DoubleColumn.create(closeTitle, close);
+    private static final DoubleColumn highColumn = DoubleColumn.create(highTitle, high);
+    private static final DoubleColumn lowColumn = DoubleColumn.create(lowTitle, low);
 
+    @Test
+    void ohlcPlotDoesNotThrowIllegalArgumentExceptionUsingDateTime() {
         LocalDateTime now = LocalDateTime.now();
         List<LocalDateTime> time = Arrays.asList(now, now.plusSeconds(5), now.plusSeconds(10));
-        List<Double> open = Arrays.asList(1d, 2d, 3d);
-        List<Double> close = Arrays.asList(1d, 2d, 3d);
-        List<Double> high = Arrays.asList(1d, 2d, 3d);
-        List<Double> low = Arrays.asList(1d, 2d, 3d);
+        DateTimeColumn dateTimeColumn = DateTimeColumn.create(timeTitle, time);
+        Table priceTable = Table.create(dateTimeColumn, openColumn, closeColumn, highColumn, lowColumn);
+        Figure figure = OHLCPlot.create(graphTitle, priceTable, timeTitle, openTitle, highTitle, lowTitle, closeTitle);
 
-        DateTimeColumn timeColumn = DateTimeColumn.create(timeTitle, time);
-        DoubleColumn openColumn = DoubleColumn.create(openTitle, open);
-        DoubleColumn closeColumn = DoubleColumn.create(closeTitle, close);
-        DoubleColumn highColumn = DoubleColumn.create(highTitle, high);
-        DoubleColumn lowColumn = DoubleColumn.create(lowTitle, low);
+        assertNotNull(figure);
+        assertDoesNotThrow(() -> IllegalArgumentException.class);
+    }
 
-        Table priceTable = Table.create(timeColumn, openColumn, closeColumn, highColumn, lowColumn);
+    @Test
+    void ohlcPlotDoesNotThrowIllegalArgumentExceptionUsingLocalDate() {
+        LocalDate now = LocalDate.now();
+        List<LocalDate> time = Arrays.asList(now, now.plusDays(5), now.plusDays(10));
+        DateColumn dateColumn = DateColumn.create(timeTitle, time);
+        Table priceTable = Table.create(dateColumn, openColumn, closeColumn, highColumn, lowColumn);
+        Figure figure = OHLCPlot.create(graphTitle, priceTable, timeTitle, openTitle, highTitle, lowTitle, closeTitle);
+
+        assertNotNull(figure);
+        assertDoesNotThrow(() -> IllegalArgumentException.class);
+    }
+
+    @Test
+    void ohlcPlotDoesNotThrowIllegalArgumentExceptionUsingInstant() {
+        Instant now = Instant.now();
+        List<Instant> time = Arrays.asList(now, now.plusSeconds(5), now.plusSeconds(10));
+        InstantColumn instantColumn = InstantColumn.create(timeTitle, time);
+        Table priceTable = Table.create(instantColumn, openColumn, closeColumn, highColumn, lowColumn);
         Figure figure = OHLCPlot.create(graphTitle, priceTable, timeTitle, openTitle, highTitle, lowTitle, closeTitle);
 
         assertNotNull(figure);
