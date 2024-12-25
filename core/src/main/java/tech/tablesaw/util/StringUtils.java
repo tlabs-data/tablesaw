1ca7e0c379ef59d39596013232e8ed1de130e0ac
ccleva <christophe.cleva@orange.fr>
2024-12-25 18:55:27 +0100

    Remove unused code from StringUtils

--- core/src/main/java/tech/tablesaw/util/StringUtils.java
+++ core/src/main/java/tech/tablesaw/util/StringUtils.java
@@ -17,8 +17,6 @@
 package tech.tablesaw.util;
 
 import com.google.common.base.Strings;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -70,130 +68,6 @@ public class StringUtils {
   private static final Pattern ZERO_DECIMAL_PATTERN = Pattern.compile("\\.0+$");
 
   private StringUtils() {}
-
-  // Empty checks
-  // -----------------------------------------------------------------------
-
-  /**
-   * Splits a String by Character type as returned by {@code java.lang.Character.getType(char)}.
-   * Groups of contiguous characters of the same type are returned as complete tokens, with the
-   * following exception: if {@code camelCase} is {@code true}, the character of type {@code
-   * Character.UPPERCASE_LETTER}, if any, immediately preceding a token of type {@code
-   * Character.LOWERCASE_LETTER} will belong to the following token rather than to the preceding, if
-   * any, {@code Character.UPPERCASE_LETTER} token.
-   *
-   * @param str the String to split, may be {@code null}
-   * @return an array of parsed Strings, {@code null} if null String input
-   * @since 2.4
-   */
-  public static String[] splitByCharacterTypeCamelCase(final String str) {
-    if (str == null) {
-      return null;
-    }
-    if (str.isEmpty()) {
-      return new String[0];
-    }
-    final char[] c = str.toCharArray();
-    final List<String> list = new ArrayList<>();
-    int tokenStart = 0;
-    int currentType = Character.getType(c[tokenStart]);
-    for (int pos = tokenStart + 1; pos < c.length; pos++) {
-      final int type = Character.getType(c[pos]);
-      if (type == currentType) {
-        continue;
-      }
-      if (type == Character.LOWERCASE_LETTER && currentType == Character.UPPERCASE_LETTER) {
-        final int newTokenStart = pos - 1;
-        if (newTokenStart != tokenStart) {
-          list.add(new String(c, tokenStart, newTokenStart - tokenStart));
-          tokenStart = newTokenStart;
-        }
-      } else {
-        list.add(new String(c, tokenStart, pos - tokenStart));
-        tokenStart = pos;
-      }
-      currentType = type;
-    }
-    list.add(new String(c, tokenStart, c.length - tokenStart));
-    return list.toArray(new String[0]);
-  }
-
-  // Joining
-  // -----------------------------------------------------------------------
-
-  /**
-   * Joins the elements of the provided array into a single String containing the provided list of
-   * elements.
-   *
-   * <p>No delimiter is added before or after the list. Null objects or empty strings within the
-   * array are represented by empty strings.
-   *
-   * <pre>
-   * StringUtils.join(null, *)               = null
-   * StringUtils.join([], *)                 = ""
-   * StringUtils.join([null], *)             = ""
-   * StringUtils.join(["a", "b", "c"], ';')  = "a;b;c"
-   * StringUtils.join(["a", "b", "c"], null) = "abc"
-   * StringUtils.join([null, "", "a"], ';')  = ";;a"
-   * </pre>
-   *
-   * @param array the array of values to join together, may be null
-   * @param separator the separator character to use
-   * @return the joined String, {@code null} if null array input
-   * @since 2.0
-   */
-  public static String join(final Object[] array, final char separator) {
-    if (array == null) {
-      return null;
-    }
-    return join(array, separator, 0, array.length);
-  }
-
-  /**
-   * Joins the elements of the provided array into a single String containing the provided list of
-   * elements.
-   *
-   * <p>No delimiter is added before or after the list. Null objects or empty strings within the
-   * array are represented by empty strings.
-   *
-   * <pre>
-   * StringUtils.join(null, *)               = null
-   * StringUtils.join([], *)                 = ""
-   * StringUtils.join([null], *)             = ""
-   * StringUtils.join(["a", "b", "c"], ';')  = "a;b;c"
-   * StringUtils.join(["a", "b", "c"], null) = "abc"
-   * StringUtils.join([null, "", "a"], ';')  = ";;a"
-   * </pre>
-   *
-   * @param array the array of values to join together, may be null
-   * @param separator the separator character to use
-   * @param startIndex the first index to start joining from. It is an error to pass in an end index
-   *     past the end of the array
-   * @param endIndex the index to stop joining from (exclusive). It is an error to pass in an end
-   *     index past the end of the array
-   * @return the joined String, {@code null} if null array input
-   * @since 2.0
-   */
-  private static String join(
-      final Object[] array, final char separator, final int startIndex, final int endIndex) {
-    if (array == null) {
-      return null;
-    }
-    final int noOfItems = endIndex - startIndex;
-    if (noOfItems <= 0) {
-      return EMPTY;
-    }
-    final StringBuilder buf = new StringBuilder(noOfItems * 16);
-    for (int i = startIndex; i < endIndex; i++) {
-      if (i > startIndex) {
-        buf.append(separator);
-      }
-      if (array[i] != null) {
-        buf.append(array[i]);
-      }
-    }
-    return buf.toString();
-  }
 
   // Padding
   // -----------------------------------------------------------------------
