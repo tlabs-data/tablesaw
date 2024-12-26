aa4266295df0f44b2c63516972ec2ce1d83a7d54
ccleva <christophe.cleva@orange.fr>
2024-12-26 19:27:33 +0100

    Additional testing for DateTime and Instant columns

--- core/src/test/java/tech/tablesaw/api/InstantColumnTest.java
+++ core/src/test/java/tech/tablesaw/api/InstantColumnTest.java
@@ -61,7 +61,7 @@ class InstantColumnTest {
   }
 
   @Test
-  public void testCountUnique() {
+  void testCountUnique() {
     InstantColumn column1 = InstantColumn.create("instants");
     column1.append(baselineInst);
     column1.append(baselineInst);
@@ -72,7 +72,7 @@ class InstantColumnTest {
   }
 
   @Test
-  public void testCustomParser() {
+  void testCustomParser() {
     class CustomParser extends AbstractColumnParser<Instant> {
 
       private final List<String> VALID_VALUES = Arrays.asList("now");
@@ -101,4 +101,106 @@ class InstantColumnTest {
     column1.appendCell("now");
     assertFalse(column1.isMissing(1));
   }
+
+  @Test
+  void testSetMissing() {
+    BooleanColumn missingBefore = instanceColumn.missingValues();
+    assertFalse(missingBefore.get(0));
+    assertFalse(missingBefore.get(1));
+    assertTrue(missingBefore.get(3));
+    instanceColumn.setMissing(0);
+    BooleanColumn missingAfter = instanceColumn.missingValues();
+    assertTrue(missingAfter.get(0));
+    assertFalse(missingAfter.get(1));
+    assertTrue(missingBefore.get(3));
+  }
+  
+  @Test
+  void removeMissing() {
+    assertEquals(3, instanceColumn.removeMissing().size());
+  }
+  
+  @Test
+  void contains() {
+    assertTrue(instanceColumn.contains(baselineInst));
+    assertFalse(instanceColumn.contains(baselineInst.plusSeconds(1_000)));
+  }
+
+  @Test
+  void testSubset() {
+    final InstantColumn subset = instanceColumn.subset(new int[] {0, 1});
+    assertEquals(2, subset.size());
+    assertEquals(beforeInst, subset.get(0));
+    assertEquals(baselineInst, subset.get(1));
+  }
+  
+  @Test
+  void testWhere() {
+    final InstantColumn subset = instanceColumn.where(instanceColumn.isNotMissing().and(instanceColumn.isBefore(afterInst)));
+    assertEquals(2, subset.size());
+    assertEquals(beforeInst, subset.get(0));
+    assertEquals(baselineInst, subset.get(1));
+  }
+
+  @Test
+  void testSetSelection() {
+    instanceColumn.set(instanceColumn.isMissing(), afterInst);
+    final InstantColumn subset = instanceColumn.where(instanceColumn.isBefore(afterInst));
+    assertEquals(2, subset.size());
+    assertEquals(beforeInst, subset.get(0));
+    assertEquals(baselineInst, subset.get(1));
+  }
+  
+  @Test
+  void testCountMissing() {
+    assertEquals(1, instanceColumn.countMissing());
+  }
+  
+  @Test
+  void testAppendColumn() {
+    assertEquals(8, instanceColumn.append(instanceColumn.copy()).size());
+  }
+
+  @Test
+  void testAppendRow() {
+    final InstantColumn appended = instanceColumn.append(instanceColumn.copy(), 3);
+    assertEquals(5, appended.size());
+    assertEquals(2, appended.countMissing());
+  }
+  
+  @Test
+  void testSetRow() {
+    final InstantColumn appended = instanceColumn.set(0, instanceColumn.copy(), 3);
+    assertEquals(4, appended.size());
+    assertEquals(2, appended.countMissing());
+  }
+  
+  @Test
+  void testTopN() {
+    final List<Instant> top = instanceColumn.top(2);
+    assertEquals(2, top.size());
+    assertEquals(afterInst, top.get(0));
+    assertEquals(baselineInst, top.get(1));
+  }
+
+  @Test
+  void testTopNAboveSize() {
+    final List<Instant> top = instanceColumn.top(10);
+    assertEquals(instanceColumn.size(), top.size());
+  }
+
+  @Test
+  void testBottomN() {
+    final List<Instant> bottom = instanceColumn.bottom(2);
+    assertEquals(2, bottom.size());
+    assertNull(bottom.get(0));
+    assertEquals(beforeInst, bottom.get(1));
+  }
+
+  @Test
+  void testBottomNAboveSize() {
+    final List<Instant> bottom = instanceColumn.bottom(instanceColumn.size() + 2);
+    assertEquals(instanceColumn.size(), bottom.size());
+  }
+
 }
