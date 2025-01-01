60bcb91c1e0ab37d0e7fa4f9cb8a1630e4a5efce
ccleva <christophe.cleva@orange.fr>
2025-01-01 15:51:56 +0100

    Strengthen And and Or for filters

--- core/src/main/java/tech/tablesaw/filtering/Or.java
+++ core/src/main/java/tech/tablesaw/filtering/Or.java
@@ -2,28 +2,34 @@ package tech.tablesaw.filtering;
 
 import com.google.common.annotations.Beta;
 import com.google.common.base.Preconditions;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import tech.tablesaw.api.Table;
 import tech.tablesaw.selection.Selection;
 
 @Beta
 public class Or implements Function<Table, Selection> {
 
-  private Function<Table, Selection>[] arguments;
+  private final List<Function<Table, Selection>> arguments;
 
   @SafeVarargs
+  @SuppressWarnings("varargs")
   public Or(Function<Table, Selection>... arguments) {
     Preconditions.checkNotNull(arguments, "The arguments to Or must be non-null");
     Preconditions.checkArgument(
         arguments.length > 0, "The arguments to Or must be an array of length 1 or greater");
-    this.arguments = arguments;
+    this.arguments = Arrays.stream(arguments).collect(Collectors.toList());
   }
 
   @Override
   public Selection apply(Table table) {
-    Selection result = arguments[0].apply(table);
-    for (int i = 1; i < arguments.length; i++) {
-      result.or(arguments[i].apply(table));
+    Selection result = arguments.get(0).apply(table);
+    for (int i = 1; i < arguments.size(); i++) {
+      result.or(arguments.get(i).apply(table));
     }
     return result;
   }
